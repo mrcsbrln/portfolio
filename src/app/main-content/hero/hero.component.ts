6517da7c70c098c54f2ce45376dc9a9e4b9d5be8
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-12-12 22:44:34 +0100

    marquee component

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { MarqueeComponent } from '../../shared/marquee/marquee.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [NavbarComponent, ButtonComponent],
+  imports: [NavbarComponent, ButtonComponent, MarqueeComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
