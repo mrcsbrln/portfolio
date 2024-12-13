003cd820c24caedd937c81020e006946b51a1d85
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-12-13 18:57:50 +0100

    update hero section

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -10,4 +10,24 @@ import { MarqueeComponent } from '../../shared/marquee/marquee.component';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  email = 'info@marcus-hartmann.net';
+  githubIcon: string = '/img/github1.svg';
+  linkedInIcon: string = '/img/linkedin1.svg';
+
+  onMouseOver(icon: string): void {
+    if (icon === 'github') {
+      this.githubIcon = '/img/github2.svg';
+    } else if (icon === 'linkedin') {
+      this.linkedInIcon = '/img/linkedin2.svg';
+    }
+  }
+
+  onMouseLeave(icon: string): void {
+    if (icon === 'github') {
+      this.githubIcon = '/img/github1.svg';
+    } else if (icon === 'linkedin') {
+      this.linkedInIcon = '/img/linkedin1.svg';
+    }
+  }
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
