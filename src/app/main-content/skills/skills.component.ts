7b4b871b4c22612427672116e8beb3cd7d40dcb4
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-12-16 21:08:36 +0100

    progress

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
