2e50158b56212c390c9dc1dab9cd7c05491f8890
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-12-24 15:15:34 +0100

    progress

--- src/app/main-content/contact/contact-form/contact-form.component.ts
+++ src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactData } from '../../../interfaces/contactData.interface';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+
+  contactData: ContactData = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  onSubmit() {
+    console.log(this.contactData);
+  }
 
 }
