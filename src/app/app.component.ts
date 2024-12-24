9877bd4aa3f3f0221bc33fd48ea447c1a520bad4
mrcsbln <hrtmnn.mrcs@gmail.com>
2024-12-24 12:15:28 +0100

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
@@ -7,7 +7,7 @@ import { NavbarComponent } from "./shared/navbar/navbar.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainContentComponent, FooterComponent, NavbarComponent],
+  imports: [RouterLink ,RouterOutlet, MainContentComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
