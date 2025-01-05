ffeac6df51fb75cf6f7371c943183d4278268835
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-05 11:58:06 +0100

    progress

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,14 +3,13 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { ReferencesComponent } from './references/references.component';
 import { ContactComponent } from './contact/contact.component';
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent, ContactComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
